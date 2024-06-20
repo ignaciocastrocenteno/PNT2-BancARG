9254d9f956710abb90cb1229ec5585ef39fd2ec4
Ignacio Castro Centeno <ignaciocastrocenteno@gmail.com>
2024-06-20 15:50:13 -0300

    Agregando nombre multi-word al componente 'Login' para evitar errores

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template lang="">
   <div>
-    <h1>Iniciá sesión o creá una cuenta en BancARG</h1>
+    <h1 class="display-1">Iniciá sesión o creá una cuenta en BancARG</h1>
     <form>
       <input v-if="isRegister" type="email" placeholder="Correo electrónico" />
       <br />
@@ -17,20 +17,21 @@
 </template>
 <script>
 export default {
+  name: "userLogin",
   data() {
     return {
       isRegister: false
     }
   },
-  created(){
+  created() {
     this.isRegister = this.modo;
   },
   methods: {
     cambiarFormulario() {
       this.isRegister = !this.isRegister
     },
-    irAPerfil(){
-      this.$router.push({name:'Perfil'})
+    irAPerfil() {
+      this.$router.push({ name: 'Perfil' })
     }
   }
 }
