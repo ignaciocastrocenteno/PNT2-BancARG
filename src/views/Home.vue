15b1fa6be4de99190df1930ffc2f8a96e632ec1a
Vartencito <104112059+Vartencito@users.noreply.github.com>
2024-06-21 10:02:26 -0300

    cambios de estilos en el home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,61 @@
 <template lang="">
-  <div>
+  <header>
     <h1>Hola, bienvenido a BancArg</h1>
-  </div>
-  <div>
-    <h3>Utilizá nuestra calculadora de monedas extranjeras a pesos!</h3>
-    <h4>Valores de las monedas:</h4>
-    <li>${{ this.monedasExtranjeras.dolarBlue }} pesos el dólar blue</li>
-    <li>${{ this.monedasExtranjeras.dolarOficial }} pesos el dólar oficial</li>
-    <li>${{ this.monedasExtranjeras.realOficial }} pesos el real oficial</li>
-    <li>${{ this.monedasExtranjeras.euroOficial }} pesos el euro oficial</li>
-    <br />
-    <br />
-    <input v-model="pesosArgentinos" type="number" placeholder="Ingresá tus pesos" />
-    <br />
-    <br />
-    <label for="monedas">Elegí la moneda:</label>
-    <select id="moneda" name="moneda" v-model="monedaSeleccionada">
-      <option :value="monedasExtranjeras.dolarOficial">Dolar Oficial</option>
-      <option :value="monedasExtranjeras.dolarBlue">Dolar Blue</option>
-      <option :value="monedasExtranjeras.realOficial">Real</option>
-      <option :value="monedasExtranjeras.euroOficial">Euro</option>
-    </select>
+    <h3 class="lemaEmpresa"><em>La mejor app para manejar tu plata</em></h3>
+    <div id="loginWrapper">
+      <!-- <h3>Para más funciones, ¡inicia sesión!</h3> -->
+      <button class="btn btn-success" @click="irALogin">Iniciar Sesión</button>
+    </div>
+  </header>
+  <main>
+    <section id="tituloCalculadoraDeMonedas">
+      <h3>Utilizá nuestra calculadora de monedas extranjeras a pesos!</h3>
+    </section>
+    <section id="valoresCalculadoraDeMonedas">
+      <h4>Valores de las monedas</h4>
+      <div id="cotizacionesActualesMonedas">
+        <li>
+          <span class="montoMoneda">${{ this.monedasExtranjeras.dolarBlue }}</span> pesos el dólar
+          blue
+        </li>
+        <li>
+          <span class="montoMoneda">${{ this.monedasExtranjeras.dolarOficial }}</span> pesos el
+          dólar oficial
+        </li>
+        <li>
+          <span class="montoMoneda">${{ this.monedasExtranjeras.realOficial }}</span> pesos el real
+          oficial
+        </li>
+        <li>
+          <span class="montoMoneda">${{ this.monedasExtranjeras.euroOficial }}</span> pesos el euro
+          oficial
+        </li>
+      </div>
+    </section>
+    <section id="montoConvertible">
+      <h4>Ingresa la cantidad de pesos que quiere convertir</h4>
+      <input
+        v-model="pesosArgentinos"
+        type="text"
+        placeholder="Monto en $ARS"
+        autocomplete="off"
+        min-length="1"
+        required
+        size="1"
+      />
+      <label for="monedas">Elegí la moneda:</label>
+      <select id="moneda" name="moneda" v-model="monedaSeleccionada" required>
+        <option :value="monedasExtranjeras.dolarOficial">Dolar Oficial</option>
+        <option :value="monedasExtranjeras.dolarBlue">Dolar Blue</option>
+        <option :value="monedasExtranjeras.realOficial">Real</option>
+        <option :value="monedasExtranjeras.euroOficial">Euro</option>
+      </select>
+    </section>
     <br />
     <button @click="calcularResultado">calcular</button>
     <br />
     <input type="text" placeholder="Resultado" v-model="resultado" />
-  </div>
-  <div>
-    <h3>Para más funciones, ¡inicia sesión!</h3>
-    <button @click="irALogin">Iniciar Sesión</button>
-  </div>
+  </main>
 </template>
 <script>
 export default {
