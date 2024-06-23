e25e3927cbcf4e0054a8b12e3411a7e815ac7acb
Ignacio Castro Centeno <ignaciocastrocenteno@gmail.com>
2024-06-23 16:33:09 -0300

    Corrección en la sintaxis del archivo

--- src/components/ManejarSaldo.vue
+++ src/components/ManejarSaldo.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div>
-      <h1>Saldo disponible: ${{ saldoTotal }}</h1>
+      <h1>Saldo disponible: USD {{ saldoTotal }}</h1>
     </div>
     <div>
       <h2>Cargar saldo</h2>
@@ -10,7 +10,12 @@
     </div>
     <div>
       <h2>Transferir saldo</h2>
-      <input v-model.number="montoTransferencia" type="number" placeholder="Monto a transferir" min="0" />
+      <input
+        v-model.number="montoTransferencia"
+        type="number"
+        placeholder="Monto a transferir"
+        min="0"
+      />
       <label for="contacto">Selecciona un contacto:</label>
       <select id="contacto" v-model="contactoSeleccionado">
         <option v-for="contacto in listaContactos" :key="contacto.cuenta" :value="contacto.cuenta">
@@ -47,48 +52,51 @@ export default {
       this.agregarSaldo = 0
     },
 
-
     async transferirSaldo() {
       if (this.montoTransferencia <= 0) {
-        this.mensaje = 'El monto a transferir debe ser mayor que $0';
-        return;
+        this.mensaje = 'El monto a transferir debe ser mayor que $0'
+        return
       }
 
       if (this.montoTransferencia > this.saldoTotal) {
-        this.mensaje = 'Transferencia fallida: Saldo insuficiente';
-        return;
+        this.mensaje = 'Transferencia fallida: Saldo insuficiente'
+        return
       }
 
-      const contacto = this.listaContactos.find(contacto => contacto.cuenta === this.contactoSeleccionado);
+      const contacto = this.listaContactos.find(
+        (contacto) => contacto.cuenta === this.contactoSeleccionado
+      )
       if (!contacto) {
-        this.mensaje = 'Contacto no encontrado';
-        return;
+        this.mensaje = 'Contacto no encontrado'
+        return
       }
 
       try {
-        this.saldoTotal -= this.montoTransferencia;
-        contacto.saldo += this.montoTransferencia;
+        this.saldoTotal -= this.montoTransferencia
+        contacto.saldo += this.montoTransferencia
 
-        const response = await fetch(`https://667360316ca902ae11b407a5.mockapi.io/api/v1/users/contactos/${contacto.id}`, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(contacto)
-        });
+        const response = await fetch(
+          `https://667360316ca902ae11b407a5.mockapi.io/api/v1/users/contactos/${contacto.id}`,
+          {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(contacto)
+          }
+        )
 
         if (!response.ok) {
-          throw new Error('Error al actualizar el contacto');
+          throw new Error('Error al actualizar el contacto')
         }
 
-        this.mensaje = `Transferencia exitosa: $${this.montoTransferencia} a ${contacto.nombre}`;
-        this.montoTransferencia = 0;
+        this.mensaje = `Transferencia exitosa: $${this.montoTransferencia} a ${contacto.nombre}`
+        this.montoTransferencia = 0
       } catch (error) {
-        this.mensaje = 'Error al realizar la transferencia';
-        console.error(this.mensaje, error);
+        this.mensaje = 'Error al realizar la transferencia'
+        console.error(this.mensaje, error)
       }
     }
-
-  },
+  }
 }
 </script>
